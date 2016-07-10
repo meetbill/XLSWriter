6cc46beb06c34b8d8a1f75f56a048ba15f5da0e1
BillWang139967 <wangbin139967@163.com>
2016-07-10 11:58:27 +0800

    update XLSWriter.py

--- XLSWriter.py
+++ XLSWriter.py
@@ -14,13 +14,15 @@ import mylib.xlwt as xlwt
 class XLSWriter(object):
     """A XLS writer that produces XLS files from unicode data.
     """
+    #{{{__init__
     def __init__(self, file, encoding='utf-8'):
         # must specify the encoding of the input data, utf-8 default.
         self.file = file
         self.encoding = encoding
         self.wbk = xlwt.Workbook()
         self.sheets = {}
-        
+    #}}}
+    #{{{add_image
     def add_image(self,bmp_name='',x='',y='',length='',title_name='ceshi',sheet_name='sheet'):
         if sheet_name not in self.sheets:
             # Create if does not exist
@@ -46,6 +48,8 @@ class XLSWriter(object):
                                                         1,length-1,\
                                                         title_name,style)
         self.sheets[sheet_name]['rows'] += 1
+    #}}}
+    #{{{add_header
     def add_header(self,header_name,length,sheet_name='sheet'):
         if sheet_name not in self.sheets:
             # Create if does not exist
@@ -65,6 +69,8 @@ class XLSWriter(object):
                                                     0,length-1,\
                                                     header_name,style)
         self.sheets[sheet_name]['rows'] += 1
+    #}}}
+    #{{{add_remark
     def add_remark(self,remark_name,length,sheet_name='sheet'):
         if sheet_name not in self.sheets:
             # Create if does not exist
@@ -72,18 +78,25 @@ class XLSWriter(object):
         style = xlwt.XFStyle() # Create Style
         font = xlwt.Font()
         font.bold = False
-        font.height = 0x00EB
+        #font.height = 0x00EB
+        font.height = 0x00FD
         style.font = font
+        #style.alignment.wrap = xlwt.Alignment.WRAP_AT_RIGHT
         alignment = xlwt.Alignment() # Create Alignment
-        alignment.horz = xlwt.Alignment.HORZ_CENTER 
+        alignment.horz = xlwt.Alignment.HORZ_LEFT
         # May be: HORZ_GENERAL,HORZ_LEFT, HORZ_CENTER, HORZ_RIGHT, HORZ_FILLED, HORZ_JUSTIFIED,HORZ_CENTER_ACROSS_SEL, HORZ_DISTRIBUTED
         alignment.vert = xlwt.Alignment.VERT_CENTER 
         # May be: VERT_TOP,VERT_CENTER, VERT_BOTTOM, VERT_JUSTIFIED, VERT_DISTRIBUTED
+        alignment.wrap = xlwt.Alignment.WRAP_AT_RIGHT
         style.alignment = alignment # Add Alignment to Style
         self.sheets[sheet_name]['sheet'].write_merge(self.sheets[sheet_name]['rows'],self.sheets[sheet_name]['rows'],\
                                                     0,length-1,\
                                                     remark_name,style)
+        tall_style = xlwt.easyxf('font:height 820;')
+        self.sheets[sheet_name]['sheet'].row(self.sheets[sheet_name]['rows']).set_style(tall_style)
         self.sheets[sheet_name]['rows'] += 1
+    #}}}
+    #{{{create_sheet
     def create_sheet(self, sheet_name='sheet'):
         """Create new sheet
         """
@@ -95,7 +108,8 @@ class XLSWriter(object):
         self.sheets[sheet_name]['index'] = sheet_index
         self.sheets[sheet_name]['sheet'] = self.wbk.add_sheet('%s%s' % (sheet_name, sheet_index if sheet_index else ''), cell_overwrite_ok=True)
         self.sheets[sheet_name]['rows'] = 0
-
+    #}}}
+    #{{{cell
     def cell(self, s):
         if isinstance(s, basestring):
             if not isinstance(s, unicode):
@@ -105,7 +119,8 @@ class XLSWriter(object):
         else:
             s = str(s)
         return s
-
+    #}}}
+    #{{{setcol_width
     def setcol_width(self, row, sheet_name='sheet'):
         if sheet_name not in self.sheets:
             # Create if does not exist
@@ -122,7 +137,9 @@ class XLSWriter(object):
                 self.writerow(self.sheets[sheet_name]['header'], sheet_name)
         for ci, width in enumerate(row):
             self.sheets[sheet_name]['sheet'].col(ci).width=256*width
-    def writerow(self, row, sheet_name='sheet',border=False,pattern=False):
+    #}}}
+    #{{{writerow
+    def writerow(self, row, sheet_name='sheet',border=False,pattern_n=0):
         style = xlwt.XFStyle()
         if border:
             borders = xlwt.Borders()
@@ -133,11 +150,11 @@ class XLSWriter(object):
             borders.bottom_colour=0x3A    
          
             style.borders = borders 
-        if pattern:
+        if pattern_n:
             pattern = xlwt.Pattern() # Create the Pattern
             pattern.pattern = xlwt.Pattern.SOLID_PATTERN 
             # May be: NO_PATTERN,SOLID_PATTERN, or 0x00 through 0x12
-            pattern.pattern_fore_colour = 22
+            pattern.pattern_fore_colour = pattern_n
             # May be: 8 through 63. 0 = Black, 1 = White, 2 = Red, 3 = Green, 4 = Blue, 
             # 5 = Yellow, 6 = Magenta, 7 = Cyan, 16 = Maroon, 17 = Dark Green, 18 = Dark Blue, 19 = Dark Yellow , 
             # almost brown), 20 = Dark Magenta, 21 = Teal, 22 = Light Gray, 23 = Dark Gray,
@@ -167,24 +184,27 @@ class XLSWriter(object):
                                                    self.cell(col) if type(col) != xlwt.ExcelFormula.Formula else col)
             #print self.sheets[sheet_name]['rows'],ci, self.cell(col) if type(col) != lib.xlwt.ExcelFormula.Formula else col
         self.sheets[sheet_name]['rows'] += 1
-            
+    #}}}
+    #{{{writerows
     def writerows(self, rows, sheet_name='sheet'):
         for row in rows:
             self.writerow(row, sheet_name)
-
+    #}}}
+    #{{{save
     def save(self):
         self.wbk.save(self.file)
-        
+    #}}}
 if __name__ == '__main__':
     # test
     xlswriter = XLSWriter('ceshi.xls')
     xlswriter.add_image("python.bmg",0,0,4,title_name=u"测试",sheet_name=u"基本信息")
     xlswriter.add_header(u"信息登记表",4,sheet_name=u"基本信息")
+    xlswriter.add_remark(u"信息登记表jkfdadaddddddddd\nddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",4,sheet_name=u"基本信息")
     xlswriter.setcol_width([20, 20, 20,20],sheet_name=u'基本信息')
 
-    xlswriter.writerow(['姓名', '年龄', '电话','QQ'],sheet_name=u'基本信息',border=True,pattern=True)
+    xlswriter.writerow(['姓名','年龄','电话','QQ'],sheet_name=u'基本信息',border=True,pattern_n=22)
     xlswriter.writerow(['张三', '30', '12345678910', '123456789'], sheet_name=u'基本信息',border=True)
-    xlswriter.writerow(['王五', '30', '13512345678', '123456789'], sheet_name=u'基本信息',border=True)
+    xlswriter.writerow(['王五', '30', '13512345678', '123456789'],sheet_name=u'基本信息',border=True,pattern_n=2)
     
     xlswriter.writerow(['检测项', '检测命令', '值','基准','状态'],sheet_name=u'服务器器状态')
     xlswriter.writerow(["磁盘空间", "df -lP | grep -e '/$' | awk '{print $5}'","20%","%85","OK"], sheet_name=u'服务器器状态')
